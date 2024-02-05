a59bb1017b5a49b92a7e90a35c68cac0bbd6f251
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2024-02-05 16:47:31 -0300

    Cria classes para segurança de senha

--- src/main/java/dev/khaled/backendtest/controllers/AuthController.java
+++ src/main/java/dev/khaled/backendtest/controllers/AuthController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.khaled.backendtest.dto.CredentialsDto;
 import dev.khaled.backendtest.dto.UserDto;
+import dev.khaled.backendtest.services.UserService;
 
 @RestController
 public class AuthController {
+
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto) {
